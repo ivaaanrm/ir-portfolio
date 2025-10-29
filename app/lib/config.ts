c9136a99c5bb9221a590fa27de497393e2dc90f6
chanRG <>
2025-10-29 16:16:33 +0100

    Refactor footer and contact components, update metadata, and enhance Lofoten travel content
    - Removed unused social media links and icons from the footer and contact page.
    - Updated the title in the metadata configuration for clarity.
    - Revised the Lofoten travel article for improved narrative flow and added new images to enhance visual storytelling.

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,6 +1,6 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Roger Gonzalez Sedano",
+  title: "RGS",
   name: "Roger",
   ogImage: "/opengraph-image.png",
   description:
@@ -8,9 +8,7 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
   github: "https://github.com/chanRG",
-  instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/in/roger-gonzalez-sedano-8b5172234/",
   email: "mailto:rogonzalezse@gmail.com",
 };
